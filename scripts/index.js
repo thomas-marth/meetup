bf30be46413f234acda5050f63e2247a572fd335
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2025-06-13 01:39:44 +0200

    Added the ability to navigate to event pages via the search input

--- scripts/index.js
+++ scripts/index.js
@@ -20,3 +20,24 @@ input.addEventListener("blur", () => {
   input.classList.remove("placeholder-focus");
   input.classList.add("placeholder-default");
 });
+
+const form = document.getElementById("search-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = input.value.trim().toLowerCase();
+  const inEvents = window.location.pathname.includes("/events/");
+
+  if (value === "mountain view, ca" || value === "mountain viev, ca") {
+    const path = inEvents
+      ? "mountain-view.html"
+      : "./events/mountain-view.html";
+    window.location.href = path;
+    return;
+  }
+
+  if (value === "new york" || value === "new york, ny") {
+    const path = inEvents ? "new-york.html" : "./events/new-york.html";
+    window.location.href = path;
+    return;
+  }
+});
